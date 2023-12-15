6e4b48cb3be0d012497f7fe8941428f7d98d5bf9
Camila Alvarez <cam.alvarez.i@gmail.com>
2023-12-15 18:03:04 -0300

    Day 7: Modified parser to include strength

--- Day_7/parser/file_parser.go
+++ Day_7/parser/file_parser.go
@@ -9,7 +9,9 @@ import (
 )
 
 type Card int
+type Strength int
 
+const numberCards int = 13
 const (
 	Two Card = iota
 	Three
@@ -26,9 +28,20 @@ const (
 	A
 )
 
+const (
+	HighCard Strength = iota
+	OnePair
+	TwoPair
+	ThreeOfAKind
+	FullHouse
+	FourOfAKind
+	FiveOfAKind
+)
+
 type Hand struct {
-	Cards [5]Card
-	Bid   int
+	Cards    [5]Card
+	Bid      int
+	Strength Strength
 }
 
 func ParseHands(file io.Reader) []Hand {
@@ -51,37 +64,83 @@ func ParseHands(file io.Reader) []Hand {
 			log.Fatal("Cannot parse number:", bid)
 		}
 		hand.Bid = b
+		var cardCounter [numberCards]int
 		for i, v := range cards {
 			switch v {
 			case '2':
 				hand.Cards[i] = Two
+				cardCounter[Two]++
 			case '3':
 				hand.Cards[i] = Three
+				cardCounter[Three]++
 			case '4':
 				hand.Cards[i] = Four
+				cardCounter[Four]++
 			case '5':
 				hand.Cards[i] = Five
+				cardCounter[Five]++
 			case '6':
 				hand.Cards[i] = Six
+				cardCounter[Six]++
 			case '7':
 				hand.Cards[i] = Seven
+				cardCounter[Seven]++
 			case '8':
 				hand.Cards[i] = Eight
+				cardCounter[Eight]++
 			case '9':
 				hand.Cards[i] = Nine
+				cardCounter[Nine]++
 			case 'T':
 				hand.Cards[i] = T
+				cardCounter[T]++
 			case 'J':
 				hand.Cards[i] = J
+				cardCounter[J]++
 			case 'Q':
 				hand.Cards[i] = Q
+				cardCounter[Q]++
 			case 'K':
 				hand.Cards[i] = K
+				cardCounter[K]++
 			case 'A':
 				hand.Cards[i] = A
+				cardCounter[A]++
 			default:
 				log.Fatal("Invalid card:", v)
 
+			}
+		}
+		var counter, sum, highestNumberCards int
+		for _, v := range cardCounter {
+			sum += v
+			if v != 0 {
+				counter++
+			}
+			if highestNumberCards < v {
+				highestNumberCards = v
+			}
+			if sum == 5 {
+				switch counter {
+				case 1:
+					hand.Strength = FiveOfAKind
+				case 2:
+					if highestNumberCards == 4 {
+						hand.Strength = FourOfAKind
+					} else {
+						hand.Strength = FullHouse
+					}
+				case 3:
+					if highestNumberCards == 3 {
+						hand.Strength = ThreeOfAKind
+					} else {
+						hand.Strength = TwoPair
+					}
+				case 4:
+					hand.Strength = OnePair
+				case 5:
+					hand.Strength = HighCard
+				}
 			}
 		}
 		hands = append(hands, hand)
